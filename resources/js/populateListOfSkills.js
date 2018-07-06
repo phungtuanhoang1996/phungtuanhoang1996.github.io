66050f2ef48da30a64255a84915146024c431cb4
Phung Tuan Hoang <phungtuanhoang1996@gmail.com>
2018-07-06 15:16:26 +0700

    styled the project cards

--- resources/js/populateListOfSkills.js
+++ resources/js/populateListOfSkills.js
@@ -12,4 +12,14 @@ const populateListOfSkills = () => {
 		$('#skill-list-ul').append(listItem)
 		console.log(skillsArray[i])
 	}
+
+	let maxWidth = 0
+
+	$(".project-card-skill-list-item").each((index, object) => {
+		if ($(object).width() > maxWidth) maxWidth = $(object).width()
+	})
+
+	$(".project-card-skill-list-item").each((index, object) => {
+		$(object).width(maxWidth + 20)
+	})
 }
