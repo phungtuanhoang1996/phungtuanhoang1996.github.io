d42bf947bc72aff75d952ccb5ecf2e8a6c8049e4
Phung Tuan Hoang <phungtuanhoang1996@gmail.com>
2018-07-08 00:46:49 +0700

    removed unnecessary scripts added a jquery method to autosize the project cards on resize added media query css for different aspect ratios of viewport

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -27,8 +27,26 @@ $(window).on("load", () => {
 		for (let i = 0; i < projectCards.length; i++) {
 			setTimeout(() => {
 				$(projectCards[i]).addClass('loaded').css('visibility', 'visible')
-
 			}, i * 200)
 		}
 	}, 500)
+})
+
+$(window).on('resize', () => {
+	console.log('image resizing')
+	let projectCards = $('.project-card')
+
+	for (let i = 0; i < projectCards.length; i++) {
+		$(projectCards[i]).css('height', 'auto')
+	}
+
+	var maxHeight = 0
+
+	for (let i = 0; i < projectCards.length; i++) {
+		if ($(projectCards[i]).height() > maxHeight) maxHeight = $(projectCards[i]).height()
+	}
+
+	for (let i = 0; i < projectCards.length; i++) {
+		$(projectCards[i]).height(maxHeight)
+	}
 })
